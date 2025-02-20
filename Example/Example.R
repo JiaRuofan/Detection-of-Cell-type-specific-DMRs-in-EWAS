80b1b28a5cd81a980a0ac2a3f5265d3bfc792c86
JiaRuofan <154356867+JiaRuofan@users.noreply.github.com>
2025-02-20 17:59:27 +0800

    Update Example.R

--- Example/Example.R
+++ Example/Example.R
@@ -7,10 +7,10 @@ source('FineDMR.R')
 
 O<-read.table('O.txt')#A n by G matrix with G observed DNA methylation levels for n samples
 X<-read.table('X.txt')#A n by Q matrix with covariates for n samples 
-
+t<-scan('t.txt') #genomic locations with length of G
 
 #########Run the FineDMR algorithm
-FD_res<-FineDMR(O,X,tj,K=6) 
+FD_res<-FineDMR(O,X,t,K=6) 
 
 #########Adjust the p values for the covariates X with Bonferroni method
 p_array_adjust<-padjust(FD_res$`p values`[,2,],method='bonferroni')
